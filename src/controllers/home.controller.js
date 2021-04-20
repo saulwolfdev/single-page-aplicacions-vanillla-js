24e4ac9f9bb76db64d7340eabc7dde979b1c3896
saulwolfdev <estrellasoscuras1984@gmail.com>
2021-04-20 13:49:58 -0300

    modal inicializacion

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,73 +1,81 @@
-import view from '../view/home.html'
+import view from "../view/home.html";
 import menu from "../mock/data";
 export default () => {
-
-  const divElement = document.createElement('div');
+  const divElement = document.createElement("div");
   divElement.innerHTML = view;
 
   const sectionsCards = divElement.querySelector(".sectionCards");
-  const sectionsButtonsContainer=divElement.querySelector(".sectionButtons");
-  
-   //CARDS
-   let diplayMenuItems=(menuItems)=>{
-      let displayMenu = menuItems.map(function (item) {
-        return `
-        <article>
-            <img src="${item.img}" alt=${item.title} class="photo" />
-              <div class="item-info">
-                <header>
-                  <h4>${item.title}</h4>
-                  <h4 class="price">$${item.price}</h4>
-                </header>
-                <p class="item-text">
-                  ${item.desc}
-                </p>
-              </div>
-            </article>
+  const sectionsButtonsContainer = divElement.querySelector(".sectionButtons");
+
+  const modalBtn = divElement.querySelector(".modal-btn");
+  const modal = divElement.querySelector(".modal-overlay");
+  const closeBtn = divElement.querySelector(".close-btn");
+
+  function modalShowInfo(item){
+   console.log("LISTA",item)
+};
+  let diplayMenuItems = (menuItems) => {
+    let displayMenu = menuItems.map(function (item) {
+    
+      return `
+        <article onclick=${modalShowInfo(item)}>
+        <div class="modalSection">
+        <img src="${item.img}" alt=${item.title} class="photo" />
+        <div class="item-info">
+          <header>
+            <h4>${item.title}</h4>
+          </header>
+        </div>
+        </div>
+      </article>
           `;
-      });
-      displayMenu = displayMenu.join("");    
-      sectionsCards.innerHTML = displayMenu;
-    }
+        
+    });
+    displayMenu = displayMenu.join("");
+    sectionsCards.innerHTML = displayMenu;
+  };
 
   //BUTTONS
-  function displayMenuButtons(){
-     
-    const categories=menu.reduce((values,item)=>{
-      if(!values.includes(item.category)){
-          values.push(item.category)
-      }
-      return values;
-    },["all"]
+  function displayMenuButtons() {
+    const categories = menu.reduce(
+      (values, item) => {
+        if (!values.includes(item.category)) {
+          values.push(item.category);
+        }
+        return values;
+      },
+      ["all"]
     );
-  
-    const selectCategoriesButtons=categories
-    .map((category)=>{
-      return `<button class="sectionButtons_Filter" type="button"  data-id=${category}>${category}</button>`
-    })
-    .join("")
-    sectionsButtonsContainer.innerHTML=selectCategoriesButtons;
-    const sectionsButtons=divElement.querySelectorAll(".sectionButtons_Filter");
-    
-    sectionsButtons.forEach(function(btn){
-      btn.addEventListener("click", function(e){
-        const category=e.currentTarget.dataset.id;
-        const menuCategory=menu.filter((menuItem)=>{
-          if(menuItem.category===category){
+
+    const selectCategoriesButtons = categories
+      .map((category) => {
+        return `<button class="sectionButtons_Filter" type="button"  data-id=${category}>${category}</button>`;
+      })
+      .join("");
+    sectionsButtonsContainer.innerHTML = selectCategoriesButtons;
+    const sectionsButtons = divElement.querySelectorAll(
+      ".sectionButtons_Filter"
+    );
+
+    sectionsButtons.forEach(function (btn) {
+      btn.addEventListener("click", function (e) {
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter((menuItem) => {
+          if (menuItem.category === category) {
             return menuItem;
           }
         });
-        if(category==="all"){
+        if (category === "all") {
           diplayMenuItems(menu);
-        }else{
+        } else {
           diplayMenuItems(menuCategory);
         }
       });
     });
-  };
-    
-  diplayMenuItems(menu);
-  displayMenuButtons()
+  }
   
+  diplayMenuItems(menu);
+  displayMenuButtons();
+ 
   return divElement;
-}
+};
