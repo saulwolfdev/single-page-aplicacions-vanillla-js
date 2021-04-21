c0c8ef9adb272ccac186a89ff6f601d93c99773b
saulwolfdev <estrellasoscuras1984@gmail.com>
2021-04-20 21:51:02 -0300

    falta modal

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -6,66 +6,25 @@ export default () => {
 
   const sectionsCards = divElement.querySelector(".sectionCards");
   const sectionsButtonsContainer = divElement.querySelector(".sectionButtons");
-  const selectModalContainer = divElement.querySelector(".sectionModals");
+
   let diplayMenuItems = (menuItems) => {
+
     let displayMenu = menuItems.map(function (item) {
+
       return `
-            <div>
-              <article class="sectionCards_Article">
+              <article class="sectionCards_Article" id="m1">
               <img src="${item.img}" alt=${item.title} class="sectionCards_Article_Img" />
                   <div class="sectionCards_Article_Container">
                   <h4 class="sectionCards_Article_Container_Title">${item.title}</h4>
                   </div>
             </article>
-            </div>
                 `;
-
     });
-    
     displayMenu = displayMenu.join("")
     sectionsCards.innerHTML = displayMenu;
-
   };
+ 
 
-  //MODAL
-/*   function displayModalItems() {
-    const modals = menu.reduce(
-      (values, item) => {
-        if (!values.includes(item.id)) {
-          values.push(item.id);
-        }
-        return values;
-      },
-      ["Todos"]
-    );
-
-    const selectModals = modals.map((id) => {
-      return `<div class="sectionModals_Items"  data-id=${id}>
-              <h5>MODAL</h5>
-      </div>`;
-    })
-      .join("");
-    selectModalContainer.innerHTML = selectModals;
-    const selectionModal = divElement.querySelectorAll(".sectionModals_Items");
-    console.log(selectionModal)
-
-    selectionModal.forEach(function (modal) {
-      modal.addEventListener("click", function (e) {
-        const idSelect = e.currentTarget.dataset.id;
-        const idMenuSelect = menu.filter((menuItem) => {
-          if (menuItem.id === idSelect) {
-            return menuItem;
-          }
-        });
-        if (idSelect === "Todos") {
-          diplayMenuItems(menu);
-        } else {
-          diplayMenuItems(idMenuSelect);
-        }
-      });
-    });
-
-  } */
 
   //BUTTONS
   function displayMenuButtons() {
